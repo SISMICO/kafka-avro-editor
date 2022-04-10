ea0688a416c54a485c2c63951c3307e4ef42ceca
Leonardo Bento <sismico@gmail.com>
2022-04-10 14:27:22 -0300

    Change log to Json Format

--- editor/src/main/kotlin/editor/kafka/KafkaSender.kt
+++ editor/src/main/kotlin/editor/kafka/KafkaSender.kt
@@ -1,12 +1,17 @@
 package editor.kafka
 
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class KafkaSender(
     val kafkaConfiguration: KafkaConfiguration = KafkaConfiguration()
 ) {
 
+    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     fun send(topic: String, message: Any) {
+        logger.info("Sending message for topic $topic")
         val producer = kafkaConfiguration.createProducer()
         val key = null
 
